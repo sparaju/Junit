4b7ca09d19a7c07772ccf36a5cd7dfcf5c951d21
SyedaFalaq123 <narmeenakb@gmail.com>
2019-12-02 19:40:12 -0600

    finalExamTestNg

---
+++ src/test/java/TestJunit1.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class TestJunit1 {
+	// global, so all methods can use
+	static WebDriver driver;
+
+	@Test
+	public void validAllBoxChecked() throws InterruptedException {
+		// Set Chrome Driver Properties
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+		// Create ChromeDriver object and launch chrome browser
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("http://techfios.com/test/101/");
+
+			//TA=ToggleAll
+		WebElement TA = driver.findElement(By.name("allbox"));
+		TA.click();
+			//AB=AllBoxes
+		WebElement AB = driver.findElement(By.xpath("//input[contains(@name,'todo')]"));
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.invisibilityOf(AB));
+		Assert.assertTrue("Error", AB.isSelected());
+		
+	}
+}
---
+++ src/test/java/TestJunit2.java
@@ -0,0 +1,37 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class TestJunit2 {
+	// global, so all methods can use
+	static WebDriver driver;
+
+	@Test
+	public void validAllBoxChecked() throws InterruptedException {
+		// Set Chrome Driver Properties
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+		// Create ChromeDriver object and launch chrome browser
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("http://techfios.com/test/101/");
+
+			//CB5=CheckBox5
+		WebElement CB5 = driver.findElement(By.name("todo[5]"));
+		CB5.click();
+			//RB=RemoveButton
+		WebElement RB = driver.findElement(By.xpath("//input[@value='Remove']"));
+		RB.click();
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.invisibilityOf(RB));
+		
+		
+	}
+}
